761b60042cde847927922b41f46c7dc719323918
Manoj Gautam2 <manojg2@chetu.com>
2021-10-19 22:29:52 +0530

    5

--- install.sh.sh
+++ install.sh.sh
@@ -1,3 +1,5 @@
-aws s3 cp s3://awsmanoj3/Test-Java/LoginWebApp-1.war /tmp
+#!/bin/bash
+
+aws s3 cp s3://awsmanoj3/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
