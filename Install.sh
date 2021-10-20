1d0bba72f625a3c59ae6fbd0383dda3ed621a148
Manoj Gautam2 <manojg2@chetu.com>
2021-10-20 16:44:13 +0530

    15

--- Install.sh
+++ Install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd /usr/share/tomcat/webapps
+cp LoginWebApp-1.war /root/codebackup
+rm -rf LoginWebApp-1.war LoginWebApp-1
+
 aws s3 cp s3://awsmanoj3/My-Project/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
--- backupcode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /usr/share/tomcat/webapps
-cp LoginWebApp-1.war /root/codebackup
-rm -rf LoginWebApp-1.war LoginWebApp-1
