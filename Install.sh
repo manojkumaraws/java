508de9466191240fc277d0821739e8b9fc9450d1
Manoj Gautam2 <manojg2@chetu.com>
2021-10-22 23:17:35 +0530

    1

--- Install.sh
+++ Install.sh
@@ -2,3 +2,8 @@
 aws s3 cp s3://awsmanoj3/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/
 sudo service tomcat restart
+
+FILE1=$(find /opt/codedeploy-agent/deployment-root/48c58548-4ca4-4db6-904f-a9a7c506c2af/ -name LoginWebApp-1.war -exec ls -tr {} \;| tail -1)
+echo $FILE1
+cp $FILE1 /usr/share/tomcat/webapps/
+ls -ltr /usr/share/tomcat/webapps/LoginWebApp-1.war
