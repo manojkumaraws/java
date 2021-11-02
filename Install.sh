bfef9b7471843acb618030f304a693bb9499c939
manojkumaraws <92542615+manojkumaraws@users.noreply.github.com>
2021-11-02 19:09:25 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,8 +7,8 @@
 #ls -ltr /usr/share/tomcat/webapps/LoginWebApp-1.war
 #systemctl restart tomcat
 
-aws s3 cp s3://awsmanoj3/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /data/apache-tomcat-9.0.54/webapps/
+#aws s3 cp s3://awsmanoj3/LoginWebApp-1.war /tmp
+#sudo mv /tmp/LoginWebApp-1.war /data/apache-tomcat-9.0.54/webapps/
 FILE1=$(find /opt/codedeploy-agent/deployment-root/48c58548-4ca4-4db6-904f-a9a7c506c2af/ -name LoginWebApp-1.war -exec ls -tr {} \;| tail -1)
 echo $FILE1
 cp $FILE1 /data/apache-tomcat-9.0.54/webapps/
